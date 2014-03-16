5dc8738d8b58150804e941ec54cb60b187e8dd87
kawamura.hryk <kawamura.hryk@gmail.com>
2014-03-16 16:40:55 +0900

    refs #2 Refactor

--- classes/controller/talksto/chatwork/about/gitlab.php
+++ classes/controller/talksto/chatwork/about/gitlab.php
@@ -34,7 +34,7 @@ class Controller_Talksto_Chatwork_About_Gitlab extends Controller
 		$api_key = $this->override('api_key', $api_key);
 
 		$things = Listener_Gitlab_Mergerequest::forge()->listen();
-		if ($this->assume_same($things))
+		if (Model_Gitlab_Mergerequest::forge()->assumes_same($things))
 		{
 			\Log::debug('ZENKINS_SAYS => I ASSUME SAME', __METHOD__);
 			return;
@@ -47,79 +47,5 @@ class Controller_Talksto_Chatwork_About_Gitlab extends Controller
 				'room_id' => $room_id,
 				'body' => $body,
 				));
-	}
-
-	private static $collections = array(
-		'id',
-		'assignee_id',
-		'state',
-		'merge_status',
-		'description',
-		);
-
-	private function assume_same(array $things)
-	{
-		if (\Arr::get($things, 'merge_status') == 'unchecked' or
-			\Arr::get($things, 'state') == 'locked')
-		{
-			return true;
-		}
-
-		$path = __DIR__.'/../../../../../data/';
-		$file = 'gitlab.merge_request';
-		! file_exists($path.$file) and \File::create($path, $file);
-		try
-		{
-			$csv = \File::read($path.$file, true);
-		}
-		catch (\Exception $e)
-		{
-			return false;
-		}
-
-		$merge_requests = \Format::forge($csv, 'csv')->to_array();
-		$ids = array();
-		if (array_key_exists('id', \Arr::get($merge_requests, 0, array())))
-		{
-			$ids = \Arr::pluck($merge_requests, 'id');
-		}
-
-		$id = \Arr::get($things, 'id');
-		$index = array_search($id, $ids);
-		$last = array();
-		if ($index !== false)
-		{
-			$fields = \Arr::get($merge_requests, $index);
-			$last = \Arr::subset($fields, static::$collections);
-		}
-
-		$now = \Arr::subset($things, static::$collections);
-		if ($last == $now) return true;
-		if (\Arr::get($now, 'state') == 'reopened')
-		{
-			\Arr::delete($merge_requests, $index);
-		}
-		else
-		{
-			if ($index !== false)
-			{
-				$merge_requests[$index] = $now;
-			}
-			else
-			{
-				$merge_requests[] = $now;
-			}
-		}
-
-		if (empty($merge_requests))
-		{
-			\File::delete($path.$file);
-		}
-		else
-		{
-			\File::update($path, $file, \Format::forge($merge_requests)->to_csv());
-		}
-
-		return false;
 	}
 }
---
+++ classes/model/gitlab/README.md
@@ -0,0 +1,12 @@
+\Initki\Model_Gitlab_*
+====
+
+クラス命名規約
+----
+
+### 単数形／複数形
+
+* GtiLab APIをデータソースとするものは当該APIのエンドポイントに合わせる
+ * APIエンドポイントが`/users`: `Model_Gitlab_Users`
+* 上記以外の場合はFuelPHP標準に合わせ、単数形とする
+
